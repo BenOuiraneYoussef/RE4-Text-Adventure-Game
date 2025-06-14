330bd867feba59bb9f572f941825401303b3549c
BenOuiraneYoussef <beastofoctober@gmail.com>
2025-06-14 05:21:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 window.addEventListener("load", () => {
-  // (PART A) GET BGM
+ 
   const bgm = document.getElementById("bgm");
-
-  // (PART B) AUTOPLAY NOT ALLOWED - SHOW "PLAY BUTTON"
   if (bgm.paused) {
-    // (B1) GET "PLAY BUTTON"
     let play = document.getElementById("play");
-
-    // (B2) CLICK TO PLAY
     play.onclick = () => {
       bgm.play();
       play.classList.remove("show");
     };
-
-    // (B3) SHOW "PLAY BUTTON"
     play.classList.add("show");
   }
 });
@@ -125,7 +118,7 @@ const textNodes = [
   },
   {
     id: 5,
-    text: '- you get cornered by the ganados in the village and end up dead because you fought them tired and low hp.',
+    text: ' you get cornered by the ganados in the village and end up dead because you fought them tired and low hp.',
     options: [
       {
         text: 'Restart',
@@ -162,7 +155,7 @@ const textNodes = [
         nextText: 10
       },
       {
-        text: 'Throw the treasure at it',
+        text: 'Throw the treasure at them',
         requiredState: (currentState) => currentState.treasure,
         nextText: 11
       }
